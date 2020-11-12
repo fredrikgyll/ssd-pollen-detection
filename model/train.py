cc9d53e5424a158e0a320836f6be559d79de7f73
Fredrik Gyllenhammar <fredrik.gyllenhammar@ntnu.no>
2020-11-12 17:25:18 +0100

    Add epoch loop

--- model/train.py
+++ model/train.py
@@ -1,7 +1,10 @@
+import pickle
 from random import randint
 import time
 import argparse
 from pathlib import Path
+from statistics import mean
+import datetime
 
 import torch
 import torch.utils.data as data
@@ -21,16 +24,25 @@ parser.add_argument(
 )
 parser.add_argument(
     '--data',
+    '-d',
     type=Path,
     help='path to data directory',
 )
 parser.add_argument(
     '--weights',
+    '-w',
     type=Path,
-    help='Path th VGG16 weights',
+    help='Path to VGG16 weights',
+)
+parser.add_argument(
+    '--save',
+    '-s',
+    type=Path,
+    help='Path to save folder',
 )
 parser.add_argument(
     '--epochs',
+    '-e',
     type=int,
     help='# of epochs to run',
 )
@@ -38,6 +50,7 @@ parser.add_argument(
     '--workers',
     type=int,
     help='# of workers in dataloader',
+    default=2,
 )
 
 
@@ -48,12 +61,16 @@ def weights_init(m):
 
 
 def train(args):
+    run_id = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    save_dir = args.save / run_id
+    save_dir.mkdir()
     batch_size = 8
 
     # Data
     root = args.data
     transforms = get_transform()
     dataset = Pollene1Dataset(root, transforms)
+    print(f'Iterations in dataset {len(dataset)//batch_size}')
     data_loader = data.DataLoader(
         dataset,
         batch_size=8,
@@ -81,35 +98,48 @@ def train(args):
     ssd_net.train()
 
     batch_iterator = iter(data_loader)
+    loss_hist = []
 
+    t0 = time.time()
     for i in range(args.epochs):
-        print(f'===== Iteration {i:2d} =====')
-        images, targets, labels = next(batch_iterator).data()
-        target_boxes = []
-        target_labels = []
-        for truth, label in zip(targets, labels):
-            target_box, target_label = encode(ssd_net.priors, truth, label)
-            target_boxes.append(target_box)
-            target_labels.append(target_label)
-        gloc = torch.stack(target_boxes, dim=0)
-        glabel = torch.stack(target_labels, dim=0).long()
-
-        if args.cuda:
-            gloc, glabel, images = gloc.cuda(), glabel.cuda(), images.cuda()
-
-        t0 = time.time()
-        ploc, pconf = ssd_net(images)
+        print(f'===== Epoch {i:2d} =====')
+        epoch_loss = []
         t1 = time.time()
-        optimizer.zero_grad()
+        for bidx, batch in enumerate(batch_iterator):
+            images, targets, labels = batch.data()
+            target_boxes = []
+            target_labels = []
+            for truth, label in zip(targets, labels):
+                target_box, target_label = encode(ssd_net.priors, truth, label)
+                target_boxes.append(target_box)
+                target_labels.append(target_label)
+            gloc = torch.stack(target_boxes, dim=0)
+            glabel = torch.stack(target_labels, dim=0).long()
 
-        loss = criterion(ploc, pconf, gloc, glabel)
-        loss.backward()
-        optimizer.step()
-        t2 = time.time()
-        print(f"Loss:\t{loss.item():3.1f}")
-        print(f"Forward:\t{t1-t0:3.1f} sec")
-        print(f"Backward:\t{t2-t1:3.1f} sec")
-        print(f"Total:\t\t{t2-t0:3.1f} sec")
+            if args.cuda:
+                gloc, glabel, images = gloc.cuda(), glabel.cuda(), images.cuda()
+
+            ploc, pconf = ssd_net(images)
+            optimizer.zero_grad()
+
+            loss = criterion(ploc, pconf, gloc, glabel)
+            loss.backward()
+            optimizer.step()
+            epoch_loss.append(loss.item())
+            if bidx % 10 == 0:
+                print(f'Loss it({bidx}): {loss.item():4.2f}')
+
+        elapsed = int(time.time() - t1)
+        print(f"Mean Loss:\t{mean(epoch_loss):4.2f}")
+        print(f"Time:\t{datetime.timedelta(seconds=elapsed)}")
+        loss_hist.extend(epoch_loss)
+        torch.save(ssd_net.state_dict(), save_dir / f'ssd_epoch_{i:02d}.pth')
+
+    elapsed = int(time.time() - t0)
+    loss_file = save_dir / 'loss_hist.pkl'
+    pickle.dump(loss_hist, loss_file.open('wb'))
+    print('====== FINISH ======')
+    print(f"Time:\t{datetime.timedelta(seconds=elapsed)}")
 
 
 if __name__ == "__main__":
