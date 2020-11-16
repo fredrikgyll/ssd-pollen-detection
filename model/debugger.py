22082f3c1883cd2199d5ab8ca63f492d7125adde
Fredrik Gyllenhammar <fredrik.gyllenhammar@ntnu.no>
2020-11-16 17:35:33 +0100

    Add evaluation method

--- model/debugger.py
+++ model/debugger.py
@@ -1,11 +1,16 @@
-import io
+from collections import namedtuple
 import torch
-from torchvision import transforms
+from torch.utils.data.dataloader import DataLoader
 from torchvision.transforms.functional import to_pil_image
 import numpy as np
 from pathlib import Path
 from PIL import Image
 import pickle
+from evaluate import evaluate
+from ssd import make_ssd
+
+from utils.augmentations import DeNormalize, get_transform
+from utils.data import Pollene1Dataset, Pollene1DatasetTest, collate
 
 
 def run_model():
@@ -13,7 +18,7 @@ def run_model():
     from utils import decode
 
     m = make_ssd()
-    x = torch.randn(1, 3, 300, 300)
+    x = torch.randn(2, 3, 300, 300)
     l, c = m(x)
     print(decode(m.priors, l.abs(), c, soft=False))
 
@@ -104,6 +109,34 @@ def data_pipeline():
     print(labels.shape)
 
 
+def evaluate_model():
+    import matplotlib.pyplot as plt
+
+    model_ch = Path('/Users/fredrikg/Projects/pollendb1/saves/2020-11-13T21-10-01.pth')
+    model = make_ssd()
+    model_state = torch.load(model_ch, map_location=torch.device('cpu'))
+    model.load_state_dict(model_state)
+
+    root = Path('/Users/fredrikg/Projects/pollendb1/data')
+
+    transforms = get_transform(train=False)
+    dataset = Pollene1DatasetTest(root, transforms)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=8,
+        shuffle=False,
+        num_workers=2,
+        collate_fn=collate,
+        pin_memory=True,
+    )
+    args = namedtuple('args', ['cuda'])
+    args.cuda = False
+    p, r = evaluate(model, data_loader, args)
+    _, ax = plt.subplots()
+    ax.plot(r, p)
+    plt.show()
+
+
 def infer(name):
     from ssd import make_ssd
     from utils.geometry import decode
@@ -112,9 +145,7 @@ def infer(name):
     import matplotlib.patches as patches
 
     dim = 300
-    model_ch = Path(
-        '/Users/fredrikg/Projects/pollendb1/saves/2020-11-12T17-43-58/ssd_epoch_19.pth'
-    )
+    model_ch = Path('/Users/fredrikg/Projects/pollendb1/saves/2020-11-13T21-10-01.pth')
     model = make_ssd()
     model_state = torch.load(model_ch, map_location=torch.device('cpu'))
     model.load_state_dict(model_state)
@@ -130,13 +161,12 @@ def infer(name):
     transform = get_transform(train=False)
 
     img, boxes, labels = transform(img, boxes, labels)
-    img, _, _ = denorm(img, boxes, labels)
     model.eval()
     with torch.no_grad():
         ploc, pconf = model(img.unsqueeze(0))
         print('done out')
-        out_boxes = decode(model.priors, ploc, pconf, soft=False, iou_thr=0.3)
-
+        out_boxes = decode(model.priors, ploc, pconf)
+    img, *_ = denorm(img, boxes, labels)
     boxes = np.clip(patch_boxes(boxes.numpy()) * dim, 0, dim)
     out_boxes = np.clip(patch_boxes(out_boxes.numpy()) * dim, 0, dim)
 
@@ -150,8 +180,96 @@ def infer(name):
     plt.show()
 
 
+def test_encoder(name):
+    from ssd import make_ssd
+    from utils import encode
+    from utils.augmentations import get_transform
+    from utils.geometry import point_form
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+
+    p = Path('/Users/fredrikg/Projects/pollendb1/data/train')
+    trf = Path('/Users/fredrikg/Projects/pollendb1/data/annotations/train_bboxes.pkl')
+    train_labels = pickle.load(trf.open('rb'))
+    boxes = train_labels[name]
+    img = Image.open(p / name)
+    trans = get_transform(train=False)
+    denorm = DeNormalize()
+    m = make_ssd()
+
+    img, boxes, label = trans(img, boxes, torch.ones(boxes.size(0)))
+    img, *_ = denorm(img, None, None)
+    target_box, target_label = encode(m.priors, boxes, label)
+    pos = target_label > 0
+    target_box = point_form(target_box.T[pos])
+
+    defaults = point_form(m.priors)[: 773 : 19 * 19, :]
+
+    chosen, chosen_idx = target_label.sort(descending=True)
+    cpos = chosen > 0
+    print(chosen_idx[cpos])
+    dim = 300
+    out_boxes = np.clip(patch_boxes(defaults.numpy()) * dim, 0, dim)
+    boxes = np.clip(patch_boxes(boxes) * dim, 0, dim)
+
+    _, ax = plt.subplots()
+    ax.imshow(to_plt_img(img), interpolation='none')
+    ax.set_title('Subsample')
+    for *xy, w, h in out_boxes:
+        ax.add_patch(
+            patches.Rectangle(xy, w, h, edgecolor='red', fill=False, alpha=0.9)
+        )
+    for *xy, w, h in boxes:
+        ax.add_patch(
+            patches.Rectangle(xy, w, h, edgecolor='green', linewidth=3, fill=False)
+        )
+    plt.show()
+
+
+def test_crit(names):
+    from ssd import make_ssd
+    from loss import MultiBoxLoss
+    from utils import encode
+    from utils.augmentations import get_transform
+    from utils.geometry import point_form
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+
+    batch_size = len(names)
+    ssd_net = make_ssd()
+    criterion = MultiBoxLoss(ssd_net.priors, batch_size)
+    p = Path('/Users/fredrikg/Projects/pollendb1/data/train')
+    trf = Path('/Users/fredrikg/Projects/pollendb1/data/annotations/train_bboxes.pkl')
+    train_labels = pickle.load(trf.open('rb'))
+    trans = get_transform(train=False)
+    ims = []
+    boxes = []
+    labels = []
+    for n in names:
+        i, b, la = trans(
+            Image.open(p / n),
+            train_labels[n],
+            torch.ones(train_labels[n].size(0)),
+        )
+        tb, tl = encode(ssd_net.priors, b, la)
+        ims.append(i)
+        boxes.append(tb)
+        labels.append(tl)
+    images = torch.stack(ims, dim=0)
+    gloc = torch.stack(boxes, dim=0)
+    print(gloc.size())
+    glabel = torch.stack(labels, dim=0).long()
+
+    ploc, pconf = ssd_net(images)
+
+    loss = criterion(ploc, pconf, gloc, glabel)
+    print(loss.item())
+
+
 if __name__ == "__main__":
     # show_img('0090_108.jpg')
     # data_pipeline()
     # run_model()
-    infer('0090_108.jpg')
+    # infer('0071_110.jpg')  # '0114_171.jpg')
+    # test_crit(['0114_171.jpg', '0403_055.jpg', '0090_108.jpg'])
+    evaluate_model()
---
+++ model/evaluate.py
@@ -0,0 +1,37 @@
+# Model, Dataloader, Ground truth
+import torch
+
+from utils.geometry import decode, jaccard
+
+
+def evaluate(model, data_loader, args):
+    model.eval()
+    confidences = []
+    predictions = []
+    nr_gts = 0
+    for batch in data_loader:
+        images, targets, labels = batch.data()
+        if args.cuda:
+            images = images.cuda()
+
+        with torch.no_grad():
+            ploc, pconf = model(images)
+            pboxes, confs, _ = decode(model.priors, ploc, pconf)  # TODO: per label
+            for box, truth, conf in zip(pboxes, targets, confs):
+                nr_gts += truth.size(0)
+                sorted_conf, order_idx = conf.sort(descending=True)
+                iou = jaccard(box[order_idx], truth)
+                preds = torch.zeros(box.size(0))
+                for ground_column in iou.split(1, 1):
+                    tp = torch.nonzero(ground_column.squeeze() > 0.5).squeeze(-1)
+                    if tp.nelement() > 0:
+                        preds[tp[0]] = 1
+                predictions.append(preds)
+                confidences.append(sorted_conf)
+    predictions = torch.cat(predictions, dim=0)
+    confidences = torch.cat(confidences, dim=0)
+    _, order = confidences.sort(descending=True)
+    predictions = torch.cumsum(predictions[order], dim=0)
+    precision = predictions / torch.arange(1, predictions.nelement() + 1)
+    recall = predictions / nr_gts
+    return precision, recall
