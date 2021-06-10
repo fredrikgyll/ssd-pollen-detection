482c8b60ad35602078eeb09909531e9fe66198f9
Fredrik Gyllenhammar <fredrik.gyllenhammar@ntnu.no>
2021-06-10 10:18:15 +0200

    Add run evaluation

--- evaluate.py
+++ evaluate.py
@@ -28,16 +28,19 @@ def interpolate(precision, recall):
         interpolation.append(np.max(pre[rec >= p]))
     return np.array(interpolation)
 
+
 def calc_map(metrics, class_subset):
     aps = ap = [metrics[cls]['average_precision'] for cls in class_subset]
     return np.mean(aps)
+
 
 def load_run_metrics(root: Path):
     files = sorted(root.glob('*.pkl'))
     metrics = [pickle.loads(f.read_bytes()) for f in files]
     return metrics
 
-def evaluate(model, dataset, class_subset, cuda=False, quiet=True):
+
+def evaluate(model, dataset, class_subset, quiet=True):
     length = len(dataset)
 
     n_gth = defaultdict(int)
@@ -45,40 +48,37 @@ def evaluate(model, dataset, class_subset, cuda=False, quiet=True):
     confidences = defaultdict(list)
 
     model.eval()
-    if cuda:
-        model = model.cuda()
+    model = model.cuda()
 
     for i in tqdm(range(length), disable=quiet):
-        file = dataset.names[i]
         try:
             image, targets = dataset[i]
         except ValueError:
-            print('error:', i, file)
             continue
+        image = image.cuda()
+        targets = targets.cuda()
         with torch.no_grad():
-            if cuda:
-                image = image.cuda()
-                targets = targets.cuda()
             detections = model(image.unsqueeze(0))
-            for j, name in enumerate(dataset.labels):
-                if name not in class_subset:
-                    continue  # skip classes
-                truth = targets[targets[:, 4] == j, :4]
-                n_gth[name] += len(truth)
-                dets = detections[0, j + 1, ...]  # 0 is bkg_label
-                mask = dets[:, 0].gt(0.0)
-                if mask.any():
-                    dets = dets[mask, ...]
-                    sorted_conf, order_idx = dets[:, 0].sort(descending=True)
-                    iou = jaccard(dets[order_idx, 1:], truth)
-                    preds = torch.zeros(dets.size(0), dtype=int)
+        for j, name in enumerate(dataset.labels):
+            if name not in class_subset:
+                continue  # skip classes
+            truth = targets[targets[:, 4] == j, :4]
+            n_gth[name] += len(truth)
+            dets = detections[0, j + 1, ...]  # 0 is bkg_label
+            mask = dets[:, 0].gt(0.0)
+            if mask.any():
+                dets = dets[mask, ...]
+                sorted_conf, order_idx = dets[:, 0].sort(descending=True)
+                iou = jaccard(dets[order_idx, 1:], truth)
+                preds = torch.zeros(dets.size(0), dtype=int)
 
-                    for ground_column in iou.split(1, 1):
-                        tp = torch.nonzero(ground_column.squeeze(-1) > 0.5).squeeze(-1)
-                        if tp.nelement() > 0:
-                            preds[tp[0]] = 1
-                    predictions[name].append(preds)
-                    confidences[name].append(sorted_conf)
+                for ground_column in iou.split(1, 1):
+                    tp = torch.nonzero(ground_column.squeeze(-1) > 0.5).squeeze(-1)
+                    if tp.nelement() > 0:
+                        preds[tp[0]] = 1
+                predictions[name].append(preds)
+                confidences[name].append(sorted_conf)
+
     metrics = {}
     for name in class_subset:
         true_pos = torch.cat(predictions[name], dim=0)
@@ -102,8 +102,32 @@ def evaluate(model, dataset, class_subset, cuda=False, quiet=True):
 
     return metrics
 
-def evaluate_run(run_id):
-    pass
+
+def evaluate_run(run_dir: Path, data_root: Path, class_subset, quiet=True):
+    checkpoints = list(run_dir.glob('ssd_*.pth'))
+    points = np.array([int(f.stem.lstrip('ssd_')) for f in checkpoints])
+
+    maps = []
+
+    transforms = SSDAugmentation(train=False)
+    test_dataset = HDF5Dataset(data_root, 'balanced1', 'test', transforms)
+    train_dataset = HDF5Dataset(data_root, 'balanced1', 'train', transforms)
+
+    datasets = [test_dataset, train_dataset]
+
+    model = make_ssd(phase='test', num_classes=len(train_dataset.labels) + 1)
+    model = model.cuda()
+
+    for i, file in enumerate(checkpoints):
+        not quiet and print(f'[{i+1:2d}/{len(checkpoints)}] {file.stem}')
+        model_state = torch.load(file, map_location=torch.device('cuda'))
+        model.load_state_dict(model_state, strict=True)
+        metrics = [evaluate(model, dst, class_subset, quiet=quiet) for dst in datasets]
+        maps.append([calc_map(m, class_subset) for m in metrics])
+    test_map, train_map = list(zip(*[np.array(m) for m in maps]))
+
+    order_idx = np.argsort(points)
+    return points[order_idx], test_map[order_idx], train_map[order_idx]
 
 
 if __name__ == "__main__":
