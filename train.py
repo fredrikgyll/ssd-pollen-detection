5014d43c7fe9c519c4604923a0b2be066caf075a
Fredrik Gyllenhammar <fredrik.gyllenhammar@ntnu.no>
2021-06-10 10:18:15 +0200

    Redefine training length by iteration

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ parser.add_argument(
 parser.add_argument('--data', '-d', type=Path, help='path to data directory')
 parser.add_argument('--weights', '-w', type=Path, help='Path to VGG16 weights')
 parser.add_argument('--save', '-s', type=Path, help='Path to save folder')
-parser.add_argument('--epochs', '-e', type=int, help='# of epochs to run')
+parser.add_argument('--iter', '-i', type=int, help='# of Iterations')
 parser.add_argument('--workers', default=2, type=int, help='# of workers in dataloader')
 parser.add_argument(
     '--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate'
@@ -53,7 +53,7 @@ parser.add_argument(
     '--multistep',
     nargs='*',
     type=int,
-    default=[40, 55],
+    default=[5000],
     help='epochs at which to decay learning rate',
 )
 parser.add_argument('--push', action='store_true', help='Push final model to bunnyCDN')
@@ -72,7 +72,7 @@ def set_default_tensor_type(tensor_type):
 
 
 def push_file(api_key: str, run_id: str, pth: Path):
-    from utils.bunny import CDNConnector
+    from model.utils.bunny import CDNConnector
 
     conn = CDNConnector(api_key, 'pollen')
     conn.upload_file('models/', pth, file_name=run_id + '.pth')
@@ -86,13 +86,12 @@ def train(args):
     logger(f'ID {run_id}')
     logger('Parameters:')
     logger(pformat(vars(args), indent=4))
-    batch_size = args.batch_size
 
     # Data
     root = args.data
+    batch_size = args.batch_size
     transforms = SSDAugmentation()
     dataset = HDF5Dataset(root, 'balanced1', 'train', transforms)
-    logger(f'Iterations in dataset {len(dataset)//batch_size}')
     data_loader = data.DataLoader(
         dataset,
         batch_size=batch_size,
@@ -102,6 +101,8 @@ def train(args):
         pin_memory=True,
     )
     num_classes = len(dataset.labels) + 1
+    logger(f'Iterations per epoch: {len(dataset)//batch_size}')
+
     # Model init
     cfg = dict(
         size=300,
@@ -110,17 +111,18 @@ def train(args):
         variances=[0.1, 0.2],
     )
     ssd_net = SSD(ResNet(backbone='resnet34', backbone_path=args.weights), cfg)
-    logger(f'Number of priors is {ssd_net.priors.size(1)}')
-    logger(f'Number of extractor layers: {len(ssd_net.loc_head)+1}')
     optimizer = SGD(
         ssd_net.parameters(),
         lr=args.lr,
         momentum=args.momentum,
         weight_decay=args.weight_decay,
     )
-    scheduler = MultiStepLR(optimizer=optimizer, milestones=args.multistep, gamma=0.1)
 
+    scheduler = MultiStepLR(optimizer=optimizer, milestones=args.multistep, gamma=0.1)
     criterion = MultiBoxLoss([0.1, 0.2], args.cuda)
+
+    logger(f'Number of priors is {ssd_net.priors.size(1)}')
+    logger(f'Number of extractor layers: {len(ssd_net.loc_head)+1}\n')
 
     if args.cuda:
         ssd_net = ssd_net.cuda()
@@ -131,57 +133,80 @@ def train(args):
 
     ssd_net.train()
 
-    loss_hist = []
+    table_widths = [10, 9, 9, 9, 9]
+    log_row = '| {{:>{}}} | {{:{}.4f}} | {{:{}.4f}} | {{:>{}}} | {{:>{}}} |'.format(
+        *table_widths
+    )
+    row_width = 4 + sum(table_widths) + 3 * (len(table_widths) - 1)
+    header = ['Iteration', 'Loc loss', 'Conf Loss', 'Elapsed', 'ETA']
+    logger('Training log'.center(row_width))
+    logger('-' * row_width)
+    logger(
+        '| '
+        + ' | '.join(f'{{:>{x}}}'.format(h) for x, h in zip(table_widths, header))
+        + ' |'
+    )
 
+    data_iter = iter(data_loader)
+    window_loss = [[], []]
+    loss_hist = [[], []]
     t0 = time.time()
-    iteration = 0
-    for i in range(args.epochs):
-        logger(f'===== Epoch {i:2d} =====')
-        epoch_loss = []
-        t1 = time.time()
-        for bidx, batch in enumerate(data_loader):
+    for i in range(1, args.iter + 1):
+        try:
+            batch = next(data_iter)
+        except StopIteration:
+            data_iter = iter(data_loader)
+            batch = next(data_iter)
+        finally:
             images, targets = batch.data()
-            with set_default_tensor_type(torch.cuda.FloatTensor):
 
-                if args.cuda:
-                    images = images.cuda()
-                    targets = [t.cuda() for t in targets]
+        with set_default_tensor_type(torch.cuda.FloatTensor):
+            if args.cuda:
+                images = images.cuda()
+                targets = [t.cuda() for t in targets]
 
-                out = ssd_net(images)
-                # [print(x.size()) for x in out]
-                # backprop
-                optimizer.zero_grad()
-                loss_l, loss_c = criterion(out, targets)
-                loss = loss_l + loss_c
+            out = ssd_net(images)
+            # backprop
+            optimizer.zero_grad()
+            loss_l, loss_c = criterion(out, targets)
+            loss = loss_l + loss_c
             loss.backward()
             optimizer.step()
             scheduler.step()
-            epoch_loss.append(loss.item())
-            if args.viz:
-                plotter.plot('loss', 'loc', 'Loss', iteration, loss_l.item())
-                plotter.plot('loss', 'conf', 'Loss', iteration, loss_c.item())
-                plotter.plot('loss', 'total', 'Loss', iteration, loss.item())
-            iteration += 1
-            if bidx % 10 == 0:
-                logger(f'Loss Iter. {bidx:02d}: {loss.item():6.3f}')
+            window_loss[0].append(loss_l.item())
+            window_loss[1].append(loss_c.item())
+        if args.viz:
+            plotter.plot('loss', 'loc', 'Loss', i, loss_l.item())
+            plotter.plot('loss', 'conf', 'Loss', i, loss_c.item())
+            plotter.plot('loss', 'total', 'Loss', i, loss.item())
+        if i % 50 == 0:
+            elapsed = int(time.time() - t0)
+            eta = int((elapsed / i) * (args.iter - i))
+            data_row = [
+                i,
+                np.mean(window_loss[0]),
+                np.mean(window_loss[1]),
+                f'{datetime.timedelta(seconds=elapsed)}',
+                f'{datetime.timedelta(seconds=eta)}',
+            ]
+            logger(log_row.format(*data_row))
+            loss_hist[0].extend(window_loss[0])
+            loss_hist[1].extend(window_loss[1])
+            window_loss = [[], []]
+            if i % 100 == 0:
+                torch.save(ssd_net.state_dict(), save_dir / f'ssd_{i:06d}.pth')
 
-        elapsed = int(time.time() - t1)
-        total_elapsed = int(time.time() - t0)
-        eta = int((total_elapsed / (i + 1)) * (args.epochs - i - 1))
-        loss_hist.extend(epoch_loss)
-        if (i + 1) % 10 == 0:
-            torch.save(ssd_net.state_dict(), save_dir / f'ssd_epoch_{i:02d}.pth')
+    loss_file: Path = save_dir / 'loss_hist.pkl'
+    loss_file.write_bytes(pickle.dumps(loss_hist))
 
-        logger(f"Mean Loss:\t{np.mean(epoch_loss):4.2f}")
-        logger(f"Time:\t{datetime.timedelta(seconds=elapsed)}")
-        logger(f"ETA:\t{datetime.timedelta(seconds=eta)}")
+    last_model_path: Path = save_dir / f'ssd_{i:06d}.pth'
+    if not last_model_path.is_file():
+        torch.save(ssd_net.state_dict(), last_model_path)
+
     elapsed = int(time.time() - t0)
-    loss_file = save_dir / 'loss_hist.pkl'
-    last_model_path = save_dir / 'ssd_last.pth'
-    torch.save(ssd_net.state_dict(), last_model_path)
-    pickle.dump(loss_hist, loss_file.open('wb'))
-    logger('====== FINISH ======')
+    logger('-' * row_width)
     logger(f"Time:\t{datetime.timedelta(seconds=elapsed)}")
+
     if args.push:
         logger('Pushing file to bunny...')
         push_file(os.getenv('BUNNY_API_KEY'), run_id, last_model_path)
